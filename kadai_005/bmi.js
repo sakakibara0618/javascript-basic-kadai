638bb762b779203e19a9a8fa37e85183896295b3
sakakibara0618 <143011317+sakakibara0618@users.noreply.github.com>
2023-11-15 19:06:48 +0900

    kadai_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,18 @@
+// 変数の宣言
+let bodyWeight;
+
+let height;
+
+let bmi;
+
+// 値の代入
+bodyWeight = '68';
+
+height = '1.7';
+
+bmi = '68';
+
+// コンソールへの出力
+console.log(bodyWeight / height * height);
+
+console.log(bmi);

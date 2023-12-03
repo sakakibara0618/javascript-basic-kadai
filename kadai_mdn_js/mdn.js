ff8928c4fb976f639431a560419bec1a75825a92
sakakibara0618 <143011317+sakakibara0618@users.noreply.github.com>
2023-12-03 21:18:33 +0900

    mdn.jsを削除

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-const date = new Date('2023, 8, 22');
-
-const year = date.getFullYear();
-const month = date.getMonth() +1;
-const day = date.getDate();
-
-console.log(year + '年' + month + '月' + day + '日');
